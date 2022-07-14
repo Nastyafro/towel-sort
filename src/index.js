fc4a51adc76922f653a1f5ad803a7bd047367f7e
Nastyafro <nastya.frolowa1994@yandex.ru>
2022-07-14 21:52:40 +0300

    feat: task is over

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let Arr = [];
+  matrix.forEach((el, i) => {
+    if(i%2 == 0 || i == 0) {
+      Arr.push(el);
+    } else {
+      Arr.push(el.reverse());
+    }
+  })
+  
+  let newArr = [];
+  Arr.map((el) => {
+    newArr = newArr.concat(el);
+  })
+  return newArr;
 }
